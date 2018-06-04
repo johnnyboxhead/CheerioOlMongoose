97fe2505f76982183ceb932d2354396b3bf111f4
Johnny <johnnyheath@hotmail.com>
2018-06-04 19:16:22 -0400

    Updated models and server to produce screen scrape and pass the data back to the Mongo DB

--- server.js
+++ server.js
@@ -27,30 +27,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+mongoose.connect("mongodb://localhost/CheerioOlMongoose");
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://outerbanksvoice.com").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $(".story").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+        .children(".hidden-xs")  
+        .children("a").next()
         .text();
       result.link = $(this)
-        .children("a")
+        .children(".hidden-xs")    
+        .children("a").next()
         .attr("href");
-
+        console.log(result)
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
@@ -59,7 +61,7 @@ app.get("/scrape", function(req, res) {
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
-          return res.json(err);
+        //   res.json(err);
         });
     });
 
